bf8d543a3735be1e44f759a04867f1336e36a875
rababyoucode <110168436+rababyoucode@users.noreply.github.com>
2022-08-04 15:11:47 +0100

    2eme calcule

--- main.c
+++ main.c
@@ -5,14 +5,18 @@ int main()
 {
     float num1 , num2 ;
     char operation , quitte;
+    /*printf("pour continue ecrir a si non ecrir q pour quitte")
+    scanf("%s",&quitte);
+    if (quitte != 'q') {  */
 
 
-    do {
+
+    do{
+
     printf("entrer le 1er nombre\n");
     scanf("%f",&num1);
     printf("entrer le 2eme nombre\n");
     scanf("%f",&num2);
-
     printf("*****\n");
 
     printf("veuillez choisir l'operation\n");
@@ -28,7 +32,7 @@ int main()
     switch (operation){
 
                      case '+':
-                          printf("LE RESULTAT EST %f", num1 + num2);
+                          printf("LE RESULTAT EST %f \n", num1 + num2);
                             break;
                      case '/' : if (num2 != 0 ){
 
@@ -46,6 +50,10 @@ int main()
                      default  : printf("votre choix est introuvable");
 
                      }
-} while (quitte == 'q' );
+
+    printf("pour continue clique a ; si non clique q pour quitte \n");
+    scanf("\n%s", &quitte);
+   }while (quitte != 'q');
+   exit(0);
     return 0;
 }
